6c29ac308743274ec9c47657edaacba1d7e2f8d1
ARUN KUMAR.A <86552741+Arun12kumar@users.noreply.github.com>
2025-09-03 07:02:41 +0530

    commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { Link } from 'expo-router'
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className=" text-7xl">Welcome</Text>
+      <Text className=" text-xl">Arun kumar code</Text>
+    </View>
+  );
+}
